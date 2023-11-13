06805eeaf9292b147dff3104760786f4183ed6b9
samuelokasiamachinemax <samuel.okasia@machinemax.com>
2023-11-13 14:56:54 +0000

    Fixed API Endpoints, and error handling

--- components_tools/geofence_formatter/geofence_formatter.jsx
+++ components_tools/geofence_formatter/geofence_formatter.jsx
@@ -6,50 +6,77 @@ import Title from "@/components /title/title";
 import './geofence_formatter.css'
 const GeofenceFormatter = () => {
     const [filename, setFilename] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [uploadedFileName, setUploadedFileName] = useState("");
 
     const API_URL_dev = "http://127.0.0.1:5000/"
-    const API_URL_prod = "https://toolsapp-bc0cf2c81289.herokuapp.com/"
+    const API_URL_prod= "https://samuelokasiamachinemax.pythonanywhere.com/"
 
-    const API_URL_dev_ext = "http://192.168.0.87:8080/"
+    const API_URL_IN_USE = API_URL_prod
+
 
     const handleUpload = async (event) => {
         const file = event.target.files[0];
+
+        // Check if a file has been selected
+        if (!file) {
+            console.log("No file selected.");
+            return; // Exit the function if no file is selected
+        }
+
+        setIsLoading(true);
+        setFilename(null);
+        setUploadedFileName(file.name); // Store the uploaded file name
+
         const formData = new FormData();
         formData.append('file', file);
-        console.log(formData)
 
+        try {
+            const response = await fetch(API_URL_dev + 'api/GeoFenceFormatter', {
+                method: 'POST',
+                body: formData,
+            });
 
-        const response = await fetch(API_URL_dev+'api/GeoFenceFormatter', {
-            method: 'POST',
-            body: formData,
-        });
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
 
-        console.log(Array.from(formData.values()));
-
-        const data = await response.json();
-        setFilename(data.filename);
+            const data = await response.json();
+            setFilename(data.filename);
+        } catch (error) {
+            console.error('Error during file upload:', error.message);
+        } finally {
+            setIsLoading(false);
+        }
     };
+
 
     return (
         <div className='geofence'>
             <div className="geofence__content">
-                <div className='geofence__content__file'>
-                    <input
-                        id="fileUpload"
-                        className="fileInput"
-                        type="file"
-                        onChange={handleUpload}
-                        style={{ display: 'none' }}
-                    />
-                    <label htmlFor="fileUpload" className="geofence__content__file--label title--sub button__box">Upload File +
-                    </label>
+                <div className="geofence__content__file__container">
 
-
-                    {filename && <a className="geofence__content__file--download label" href={`${API_URL_dev}api/download/${filename}`} download>Download Processed File</a>}
+                    <div className='geofence__content__file'>
+                            <input
+                                id="fileUpload"
+                                className="fileInput"
+                                type="file"
+                                onChange={handleUpload}
+                                style={{ display: 'none' }}
+                            />
+                            <label htmlFor="fileUpload" className="geofence__content__file--label title--sub button__box">Upload File +
+                            </label>
+                    </div>
+                    {uploadedFileName && <p className="label">{uploadedFileName}</p>} {/* Display uploaded file name */}
                 </div>
+
+                <div className="geofence__content__file__download">
+                    {isLoading && <a className="geofence__content__file__download--download label">Loading...</a>} {/* Display loading indicator */}
+
+                    {filename && <a className="geofence__content__file--download label" href={`${API_URL_IN_USE}api/download/${filename}`} download>Download Processed File {filename}</a>}
+                </div>
+
             </div>
-
-
 
         </div>
     );
--- components_tools/subgroup_filter/subgroup_filter.jsx
+++ components_tools/subgroup_filter/subgroup_filter.jsx
@@ -14,6 +14,8 @@ const SubgroupFilter = () => {
     const [selectedKey, setSelectedKey] = useState('');
     const [subOrganizations, setSubOrganizations] = useState([]);
 
+    const [isLoading, setIsLoading] = useState(false);
+
 
 
     useEffect(() => {
@@ -58,22 +60,36 @@ const SubgroupFilter = () => {
     const [filename, setFilename] = useState("");
     const handleUpload = async (event) => {
         const file = event.target.files[0];
+
+        // Check if a file has been selected
+        if (!file) {
+            console.log("No file selected.");
+            return; // Exit the function if no file is selected
+        }
+
+        setIsLoading(true); // Start loading
         const formData = new FormData();
         formData.append('file', file);
-        console.log(formData)
 
+        try {
+            const response = await fetch(API_URL_prod + `/api/CustomerFilter/${subOrganizations}`, {
+                method: 'POST',
+                body: formData,
+            });
 
-        const response = await fetch(API_URL_prod+`/api/CustomerFilter/${subOrganizations}`, {
-            method: 'POST',
-            body: formData,
-        });
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
 
-        console.log(Array.from(formData.values()));
-
-        const data = await response.json();
-        setFilename(data.filename);
+            const data = await response.json();
+            setFilename(data.filename);
+        } catch (error) {
+            console.error('Error during file upload:', error.message);
+            // Optionally, you can handle the error in the UI, e.g., by setting an error state and displaying it
+        } finally {
+            setIsLoading(false); // Stop loading
+        }
     };
-
 
 
     return (
@@ -102,10 +118,9 @@ const SubgroupFilter = () => {
                     />
                     <label htmlFor="fileUpload" className="geofence__content__file--label title--sub button__box">Upload File +
                     </label>
-
-
-                    {filename && <a className="subgrpfltr__upload--download label" href={`${API_URL_prod}api/download/${filename}`} download>Download Processed File</a>}
                 </div>
+                {isLoading && <p className="subgrpfltr__upload--loading label">Loading...</p>}
+                {filename && <a className="subgrpfltr__upload--download label" href={`${API_URL_prod}api/download/${filename}`} download>Download Processed File</a>}
             </div>
 
 
