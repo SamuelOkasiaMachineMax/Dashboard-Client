0996c1272d0aedb7d37bda565c808ed44f8d3980
samuelokasiamachinemax <samuel.okasia@machinemax.com>
2024-01-15 11:41:50 +0000

    Fixed Sub Org Filter

--- components_tools/subgroup_filter/subgroup_filter.jsx
+++ components_tools/subgroup_filter/subgroup_filter.jsx
@@ -1,121 +1,105 @@
 import React, { useEffect, useState } from 'react';
-
-import "./subgroup_filter.css"
-
 import Select from 'react-select';
 import { ClipLoader } from "react-spinners";
-
+import "./subgroup_filter.css";
 
 const SubgroupFilter = () => {
-
-    const API_URL_dev = "http://127.0.0.1:5000/"
-    const API_URL_prod= "https://samuelokasiamachinemax.pythonanywhere.com/"
-
-    const API_IN_USE = API_URL_prod
+    const API_URL_dev = "http://127.0.0.1:5000/";
+    const API_URL_prod = "https://samuelokasiamachinemax.pythonanywhere.com/";
+    const API_IN_USE = API_URL_prod;
 
     const [customerData, setCustomerData] = useState({});
     const [selectedKey, setSelectedKey] = useState('');
     const [subOrganizations, setSubOrganizations] = useState([]);
     const [filename, setFilename] = useState("");
-
-
     const [isLoading, setIsLoading] = useState(false);
-
+    const [optionsLoading, setOptionsLoading] = useState(false);
 
 
     useEffect(() => {
-        // Replace with your actual endpoint URL
-        fetch(API_IN_USE+`OrgToSubOrg`, {
+        setOptionsLoading(true);
+        fetch(API_IN_USE + `OrgToSubOrg`, {
             method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
+            headers: { 'Content-Type': 'application/json' },
         })
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
+            .then(response => {
+                if (!response.ok) throw new Error('Network response was not ok');
                 return response.json();
             })
-            .then((data) => {
+            .then(data => {
                 setCustomerData(data);
+                setOptionsLoading(false); // Stop loading once data is fetched
             })
-            .catch((error) => {
-                console.error('There was a problem with the fetch operation:', error);
+            .catch(error => {
+                console.error('Fetch operation error:', error);
+                setOptionsLoading(false); // Stop loading in case of error
             });
-    }, []); // Dependencies array is empty, meaning this effect runs once after the initial render
+    }, []);
 
-
-    const options = Object.keys(customerData).map((key) => ({
-        value: key,
-        label: key
-    }));
-
-    const handleChange = (selectedOption) => {
+    const handleSelectChange = selectedOption => {
         if (selectedOption) {
             setSelectedKey(selectedOption.value);
             setSubOrganizations(customerData[selectedOption.value]);
-            setFilename(null)
-            console.log(subOrganizations)
+            setFilename("");
         } else {
             setSelectedKey('');
             setSubOrganizations([]);
+            setFilename("");
         }
     };
+    const handleUpload = async event => {
+        const fileInput = event.target;
 
-    const handleUpload = async (event) => {
-        const file = event.target.files[0];
-
-        // Check if a file has been selected
-        if (!file) {
-            console.log("No file selected.");
-            return; // Exit the function if no file is selected
+        if (!fileInput.files || fileInput.files.length === 0) {
+            console.log("No file selected or file input is not correctly defined.");
+            return;
         }
 
-        setIsLoading(true); // Start loading
+        const originalFile = fileInput.files[0];
+        const newFileName = selectedKey ? `${selectedKey}_${originalFile.name}` : originalFile.name;
+        const modifiedFile = new File([originalFile], newFileName, { type: originalFile.type });
+
+        setIsLoading(true);
 
         const formData = new FormData();
-        formData.append('file', file);
-
-        // Add the JSON data to the FormData
+        formData.append('file', modifiedFile);
         formData.append('data', JSON.stringify({ value: subOrganizations }));
 
         try {
             const response = await fetch(API_IN_USE + 'api/CustomerFilter', {
                 method: 'POST',
-                body: formData, // Send the FormData
+                body: formData,
             });
 
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
+            if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
 
             const data = await response.json();
             setFilename(data.filename);
         } catch (error) {
-            console.error('Error during file upload:', error.message);
-            // Optionally, handle the error in the UI
+            console.error('File upload error:', error.message);
         } finally {
-            setIsLoading(false); // Stop loading
+            setIsLoading(false);
+            fileInput.value = ''; // Reset the file input
         }
     };
-
 
 
     return (
         <div className="subgrpfltr section__padding">
             <div className="subgrpfltr__select">
-                <Select
-                    options={options}
-                    onChange={handleChange}
-                    isClearable={true}
-                    isSearchable={true}
-                    placeholder="Select Organisation"
-                    className="title--sub"
-                />
-
+                {optionsLoading ? (
+                    <ClipLoader color="#1976d2" loading={optionsLoading} />
+                ) : (
+                    <Select
+                        options={Object.keys(customerData).map(key => ({ value: key, label: key }))}
+                        onChange={handleSelectChange} // Use handleSelectChange here
+                        isClearable={true}
+                        isSearchable={true}
+                        placeholder="Select Organisation"
+                        className="title--sub"
+                    />
+                )}
                 <p className="label">{subOrganizations.length} Sub Organisations Found</p>
-
             </div>
             <div className="subgrpfltr__upload">
                 <div className='subgrpfltr__upload__file'>
@@ -123,23 +107,20 @@ const SubgroupFilter = () => {
                         id="fileUpload"
                         className="fileInput"
                         type="file"
-                        onChange={handleUpload}
+                        onChange={handleUpload} // Keep handleUpload here
                         style={{ display: 'none' }}
                     />
-                    <label htmlFor="fileUpload" className="geofence__content__file--label title--sub button__box">Upload File +
+                    <label htmlFor="fileUpload" className="geofence__content__file--label title--sub button__box">
+                        Upload File +
                     </label>
                 </div>
-                {/*{isLoading && <p className="subgrpfltr__upload--loading label">Loading...</p>}*/}
 
-                {isLoading && <ClipLoader color="#1976d2" loading={isLoading}/>}
-                {filename && <a className="subgrpfltr__upload--download label" href={`${API_URL_prod}api/download/${filename}`} download>Download Processed File</a>}
+                {isLoading && <ClipLoader color="#1976d2" loading={isLoading} />}
+                {filename &&
+                    <a className="subgrpfltr__upload--download label" href ={`${API_IN_USE}api/download/${filename}`} download>{filename}</a>}
             </div>
-
-
-
         </div>
     );
 };
-
 
 export default SubgroupFilter;
